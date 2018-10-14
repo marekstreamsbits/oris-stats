3cc985778cf1121b849448e2c144e9647ddbee56
Marek Sunda <sundamar@fel.cvut.cz>
2018-10-14 20:04:53 +0200

    spring base testing introduced; minor refactor in oris extraction service - moved in two separate service for ease of testing and possibility to hide db/file extrsaction behind a new interface in case

---
+++ src/test/java/test/spring/SpringTestConfiguration.java
@@ -0,0 +1,12 @@
+package test.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:/application-TEST.properties")
+public class SpringTestConfiguration {
+
+
+
+}
