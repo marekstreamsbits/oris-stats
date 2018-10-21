1dcca75354b59e0f831a9df425339c3ed966a279
Marek Sunda <sundamar@fel.cvut.cz>
2018-10-21 21:51:57 +0200

    renamed property in api model, lombok used in api model now

--- src/main/java/oris/model/api/AttendeeStatistics.java
+++ src/main/java/oris/model/api/AttendeeStatistics.java
@@ -1,26 +1,13 @@
 package oris.model.api;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Data
 public class AttendeeStatistics {
 
     private String regNo;
     private List<EventResult> eventResults = new ArrayList<>();
-
-    public List<EventResult> getEventResults() {
-        return eventResults;
-    }
-
-    public void setEventResults(List<EventResult> eventResults) {
-        this.eventResults = eventResults;
-    }
-
-    public String getRegNo() {
-        return regNo;
-    }
-
-    public void setRegNo(String regNo) {
-        this.regNo = regNo;
-    }
 }
--- src/main/java/oris/model/api/EventResult.java
+++ src/main/java/oris/model/api/EventResult.java
@@ -1,7 +1,10 @@
 package oris.model.api;
 
+import lombok.Data;
+
 import java.util.Date;
 
+@Data
 public class EventResult {
 
     private Long eventId;
@@ -12,7 +15,7 @@ public class EventResult {
 
     private Integer attendeesInCategory;
 
-    private Integer getAttendeesInCategoryWithDisq;
+    private Integer attendeesInCategoryWithDisq;
 
     private Integer time;
 
@@ -22,87 +25,15 @@ public class EventResult {
 
     private Date date;
 
-    public EventResult(Long eventId, String eventName, Integer place, Integer attendeesInCategory, Integer getAttendeesInCategoryWithDisq, Integer time, Integer loss, String category, Date date) {
+    public EventResult(Long eventId, String eventName, Integer place, Integer attendeesInCategory, Integer attendeesInCategoryWithDisq, Integer time, Integer loss, String category, Date date) {
         this.eventId = eventId;
         this.eventName = eventName;
         this.place = place;
         this.attendeesInCategory = attendeesInCategory;
-        this.getAttendeesInCategoryWithDisq = getAttendeesInCategoryWithDisq;
+        this.attendeesInCategoryWithDisq = attendeesInCategoryWithDisq;
         this.time = time;
         this.loss = loss;
         this.category = category;
-        this.date = date;
-    }
-
-    public Long getEventId() {
-        return eventId;
-    }
-
-    public void setEventId(Long eventId) {
-        this.eventId = eventId;
-    }
-
-    public String getEventName() {
-        return eventName;
-    }
-
-    public void setEventName(String eventName) {
-        this.eventName = eventName;
-    }
-
-    public Integer getPlace() {
-        return place;
-    }
-
-    public void setPlace(Integer place) {
-        this.place = place;
-    }
-
-    public Integer getAttendeesInCategory() {
-        return attendeesInCategory;
-    }
-
-    public void setAttendeesInCategory(Integer attendeesInCategory) {
-        this.attendeesInCategory = attendeesInCategory;
-    }
-
-    public Integer getGetAttendeesInCategoryWithDisq() {
-        return getAttendeesInCategoryWithDisq;
-    }
-
-    public void setGetAttendeesInCategoryWithDisq(Integer getAttendeesInCategoryWithDisq) {
-        this.getAttendeesInCategoryWithDisq = getAttendeesInCategoryWithDisq;
-    }
-
-    public Integer getTime() {
-        return time;
-    }
-
-    public void setTime(Integer time) {
-        this.time = time;
-    }
-
-    public Integer getLoss() {
-        return loss;
-    }
-
-    public void setLoss(Integer loss) {
-        this.loss = loss;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
         this.date = date;
     }
 }
